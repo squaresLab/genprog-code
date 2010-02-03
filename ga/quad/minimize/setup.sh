fdace96e73857b72d64f5236dd9c67c7b5e7b109
Eric Schulte <eschulte@grammatech.com>
2010-02-03 04:04:43 +0000

    removing  genprog-code/trunk/ga -- with was missing many commits

--- ga/quad/minimize/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# requires: best.c baseline.c
-date > minimized.log
-../../cdiff --generate mymin baseline.c best.c 
-delta -suffix=.diff -test=./test-diff -cp_minimal=min.diff < mymin.diff 
-../../cdiff --use mymin min.diff > minimized.c
-date >> minimized.log
