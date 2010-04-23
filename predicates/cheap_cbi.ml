99beda9eddcccb75d447a614a0c4a5825d455328
Claire Le Goues <claire.legoues@gmail.com>
2010-04-23 18:50:37 +0000

    closer to properly handling scalar pairs, at least in the print out. Still need to deal with globals, CIL-inserted variables, and proper type printing-out.

--- predicates/cheap_cbi.ml
+++ predicates/cheap_cbi.ml
@@ -13,11 +13,45 @@ let fopen = Lval((Var fopen_va), NoOffset)
 let fflush = Lval((Var fflush_va), NoOffset)
 let stderr = Lval((Var stderr_va), NoOffset)
 
+let do_pa = ref false 
+
 (* the schemes: *)
 let do_returns = ref false
 let do_branches = ref false
 let do_sk = ref false
 let do_all = ref false
+
+(* Hashtbl for globals *)
+let global_vars = Hashtbl.create 100
+
+(* stolen from rmtmps.ml in Cil because I can't figure out how to
+   reference it *)
+let uninteresting =
+  let names = [
+    (* Cil.makeTempVar *)
+    "__cil_tmp";
+    
+    (* sm: I don't know where it comes from but these show up all over. *)
+    (* this doesn't seem to do what I wanted.. *)
+    "iter";
+
+    (* various macros in glibc's <bits/string2.h> *)		   
+    "__result";
+    "__s"; "__s1"; "__s2";
+    "__s1_len"; "__s2_len";
+    "__retval"; "__len";
+
+    (* various macros in glibc's <ctype.h> *)
+    "__c"; "__res";
+
+    (* We remove the __malloc variables *)
+  ] in
+
+  (* optional alpha renaming *)
+  let alpha = "\\(___[0-9]+\\)?" in
+  
+  let pattern = "\\(" ^ (String.concat "\\|" names) ^ "\\)" ^ alpha ^ "$" in
+  Str.regexp pattern
 
 (* This visitor stuff is taken from coverage and walks over the C program
  * AST and builds the hashtable that maps integers to statements. *) 
@@ -98,7 +132,7 @@ let get_next_site scheme exp l =
     incr site ;
     Hashtbl.add site_ht count (l,scheme,exp);
     let str = (sprintf "%d" count)^",%d\n" in
-      (Const(CStr(str)))
+      (count, (Const(CStr(str))))
 
 (* predicates now mean "sites", more or less *)
 
@@ -126,7 +160,7 @@ let make_printf_stmt do_flush args =
     {(mkStmt stkind) with labels = make_label()}
       
 let instr_branch e1 l s =
-  let str_exp = get_next_site "branches" e1 l in
+  let _,str_exp = get_next_site "branches" e1 l in
     make_printf_stmt true [str_exp;e1]
     
 let sig_ret_instrs () = 
@@ -137,7 +171,9 @@ let instr_rets e l s =
   let conds = 
     List.map (fun cmp -> BinOp(cmp,e,zero,(TInt(IInt,[])))) [Lt;Gt;Eq] in
   let exp_and_conds = 
-    List.map (fun cond -> cond, (get_next_site "returns" cond l)) conds in
+    List.map (fun cond -> 
+				let _, s = get_next_site "returns" cond l in 
+				  (cond, s)) conds in
   let instrs1 =
     List.map (fun (str_exp,cond) ->
 		make_printf_instr [str_exp;cond]) exp_and_conds in
@@ -145,15 +181,33 @@ let instr_rets e l s =
   let instr_list : Cil.instr list = instrs1 @ instrs2 in
     {(mkStmt (Instr(instr_list))) with labels = make_label()}
 
-let print_var vinfo =
-  (* CHECK ME: right now we're printing out all variables as
-   * integers. I believe this is reasonable, but I should check! *)
-  let name_str = Const(CStr((sprintf "%s" vinfo.vname)^",%d\n")) in 
-    make_printf_instr [name_str;Const(CStr(vinfo.vname))]
-
-class instrumentVisitor = object
+class instrumentVisitor = object(self)
   inherit nopCilVisitor
 
+  val local_vars = Hashtbl.create 100
+
+  method print_vars first_var =
+	(* currently ignoring globals entirely *)
+	let count, _ = get_next_site "scalar-pairs" (Lval(Var(first_var), NoOffset)) !currentLoc in 
+	let one_var vi =
+	  (* CHECK ME: right now we're printing out all variables as
+	   * integers. I believe this is reasonable, but I should check! *)
+	  let name_str = Const(CStr((sprintf "%d,%s" count vi.vname)^",%d\n")) in 
+		make_printf_instr [name_str;Lval(Var(vi),NoOffset)]
+	in
+	let first_instr = one_var first_var in
+	let local_print =  (* FIXME: WE'RE NOT DOING GLOBALS YET *)
+	  Hashtbl.fold 
+		(fun vname -> 
+		   fun vi -> 
+			 fun so_far_list -> 
+			   if not (vi.vname = first_var.vname) then 
+				 (one_var vi) :: so_far_list
+			   else so_far_list)
+		local_vars [] in
+	  first_instr :: local_print @ (flush_instr() :: [])
+
+(* USE Cil.var to make lvals out of varinfos *)
   method vstmt s = 
     (* insert before takes the result of a function to a statement to
      * generate a list of new statements, and then makes a new statement
@@ -173,30 +227,44 @@ class instrumentVisitor = object
              | Return(None, l) -> insert_before (mkStmt (Instr(sig_ret_instrs ()))) s  
 	     | _ -> s)
 
-  method vfunc fdec =
-    (* first, signify that we've entered a function *)
-    let enter_formals = 
-      List.map (fun str -> make_printf_instr [Const(CStr(str))]) ["function enter\n";"formal params\n"] in
-    (* introduce the print the values of the formal parameters *)
-    let param_intros = List.map print_var fdec.sformals in
-    (* next, introduce local variables with values *)
-    let intro_locals = make_printf_instr [Const(CStr("local intros\n"))] in
-    let local_intros = List.map print_var fdec.slocals in
-    let done_intro = make_printf_instr [Const(CStr("done with intros\n"))] in
-    let big_list = (enter_formals @ param_intros) @ (intro_locals :: local_intros) @ ([done_intro; (flush_instr ())]) in
-    let st = mkStmt (Instr(big_list)) in
-      ChangeDoChildrenPost
-	(fdec, fun fdec -> fdec.sbody.bstmts <- st :: fdec.sbody.bstmts; fdec) 
+  method vglob g =
+	match g with
+	  | GVarDecl(vi, l)
+	  | GVar (vi,_,l) -> Hashtbl.add global_vars vi.vname vi; DoChildren
+	  | _ -> DoChildren
 
-(* how to deal with aliasing? blargh *)
+  method vfunc fdec =
+	Hashtbl.clear local_vars;
+	List.iter
+	  (fun vi -> 
+		 Hashtbl.add local_vars vi.vname vi) fdec.sformals;
+      ChangeDoChildrenPost
+		(fdec, fun fdec -> (Hashtbl.clear local_vars; fdec))
+
+(* am I dealing with aliasing appropriately? *)
 
   method vinst i = 
-    let ilist = 
-      match i with 
-	  Set((Var(vi), off), _, l)
-	| Call(Some(Var(vi), off), _,_, l) -> [i; (print_var vi)]
-	| _ -> [i] in
-      ChangeTo ilist
+    if !do_sk then begin
+      let ilist = 
+		match i with 
+			Set((h,o), e, l) 
+		  | Call(Some((h,o)), e, _, l) ->
+			  (match h with 
+				   Var(vi) ->
+					 if not (Str.string_match uninteresting
+							   vi.vname 0) then 
+					   begin
+						 if not (Hashtbl.mem local_vars vi.vname) then
+						   Hashtbl.add local_vars vi.vname vi;
+						 let ps = self#print_vars vi in
+						   (i :: ps) @ [flush_instr()]
+					   end else [i]
+				 | _ -> [i])
+		  | _ -> [i] in
+		ChangeTo ilist
+    end
+    else DoChildren
+
 end
 
 let ins_visitor = new instrumentVisitor
@@ -204,32 +272,27 @@ let num_visitor = new numVisitor
 
 let main () = begin
   let usageMsg = "Prototype Cheap Bug Isolation Instrumentation\n" in
-  let do_pa = ref false in
   let do_cov = ref false in
 
   let filenames = ref [] in
     (* question: if we're trying to mirror coverage.ml, should we just
-       include it so that changes are consistent between files? Hm. *)
-
-  (* FIXME: fix the spaces in the argument descriptions here *)
+       include it so that changes are consistent between files? *)
 
   let argDescr = [ 
     "--returns", Arg.Set do_returns, " Instrument return values.";
     "--branches", Arg.Set do_branches, " Instrument branches.";
     "--sk", Arg.Set do_sk, " Instrument scalar-pairs.";
     "--default", Arg.Set do_all, " Do all three.";
-    "--cov", Arg.Set do_cov, " track information that would be computed by coverage.
-                               Does the --calls and --empty options to
+    "--cov", Arg.Set do_cov, " track information that would be computed by coverage. \
+                               Does the --calls and --empty options to \
                                coverage by default, but not --every-instr." ;
-    "--pa", Arg.Set do_pa, " do static pointer analysis to reduce
-                             overhead of comparing all variables to
-                             one another. For the time being, I'm pretty 
-                             sure that this only works if you're only
-                             instrumenting *one* file, so don't be stupid 
-                             about it." 
   ] in
   let handleArg str = filenames := str :: !filenames in
     Arg.parse (Arg.align argDescr) handleArg usageMsg ;
+
+	(* sometimes the ocaml type system is inexcusably stupid. Those
+	   times usually involve objects *)
+	let coerce iv = (iv : instrumentVisitor :> Cil.cilVisitor) in
 
     if !do_all then begin
       do_returns := true; do_branches := true; do_sk := true
@@ -237,58 +300,40 @@ let main () = begin
       
     Cil.initCIL();
 
-(* this map has several purposes:
- * 1) make a list of cil files from the filenames passed in 
- * 2) convert them all to CFG form
- * 3) number them properly for the coverage hashtable info
- * 4) add all defined global variables into a table.
- * 5) build pointer analysis table, if specified 
- * questions: I can only do for one file at a time b/c we don't clear
- * state, I guess? 
- *)
-    let files = List.map 
+    List.map 
       (fun filename -> 
+		 let file = Frontc.parse filename () in
+		   (* equivalent of do_cfg option to coverage *)
+		   Partial.calls_end_basic_blocks file;
+		   Cfg.computeFileCFG file;
 
-	 let file = Frontc.parse filename () in
-	   (* equivalent of do_cfg option to coverage *)
-	   Partial.calls_end_basic_blocks file;
-	   Cfg.computeFileCFG file;
-
-	   visitCilFileSameGlobals num_visitor file ; 
-	   Ptranal.analyze_file file;
-
-	   file) !filenames in
-
-      List.iter 
-	(fun file -> 
-	   begin
-	     visitCilFileSameGlobals ins_visitor file;
+		   visitCilFileSameGlobals num_visitor file ; 
+		   visitCilFileSameGlobals (coerce ins_visitor) file;
 	     
-	     let new_global = GVarDecl(stderr_va,!currentLoc) in 
-	       file.globals <- new_global :: file.globals ;
+		   let new_global = GVarDecl(stderr_va,!currentLoc) in 
+			 file.globals <- new_global :: file.globals ;
 	       
-	       let fd = Cil.getGlobInit file in 
-	       let lhs = (Var(stderr_va),NoOffset) in 
-	       let data_str = file.fileName ^ ".preds" in 
-	       let str_exp = Const(CStr(data_str)) in 
-	       let str_exp2 = Const(CStr("wb")) in 
-	       let instr = Call((Some(lhs)),fopen,[str_exp;str_exp2],!currentLoc) in 
-	       let new_stmt = Cil.mkStmt (Instr[instr]) in 
-	       let new_stmt = {new_stmt with labels = make_label()} in 
-		 fd.sbody.bstmts <- new_stmt :: fd.sbody.bstmts ; 
-		 iterGlobals file (fun glob ->
-				     dumpGlobal defaultCilPrinter stdout glob ;
-				  ) ; 
-
-		 let sites = file.fileName ^ ".sites" in
-		 let fout = open_out_bin sites in
-		   Marshal.to_channel fout site_ht [] ;
-		   Marshal.to_channel fout coverage_ht [] ;
-		   (* FIXME: this shouldn't affect reading it back in,
-		      yet, at least; but CHECK *)
-		   close_out fout ;
-	   end
-	) files;
+			 let fd = Cil.getGlobInit file in 
+			 let lhs = (Var(stderr_va),NoOffset) in 
+			 let data_str = file.fileName ^ ".preds" in 
+			 let str_exp = Const(CStr(data_str)) in 
+			 let str_exp2 = Const(CStr("wb")) in 
+			 let instr = Call((Some(lhs)),fopen,[str_exp;str_exp2],!currentLoc) in 
+			 let new_stmt = Cil.mkStmt (Instr[instr]) in 
+			 let new_stmt = {new_stmt with labels = make_label()} in 
+			   fd.sbody.bstmts <- new_stmt :: fd.sbody.bstmts ; 
+			   iterGlobals file (fun glob ->
+								   dumpGlobal defaultCilPrinter stdout glob ;
+								) ; 
+			   
+			   let sites = file.fileName ^ ".sites" in
+			   let fout = open_out_bin sites in
+				 Marshal.to_channel fout site_ht [] ;
+				 Marshal.to_channel fout coverage_ht [] ;
+				 (* FIXME: this shouldn't affect reading it back in,
+					yet, at least; but CHECK *)
+				 close_out fout) !filenames;
+				   
 end ;;
 
 main () ;;
