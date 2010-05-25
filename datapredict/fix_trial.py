7ec1ab13bbb23e41ad37a2d88e04838f9edc2f81
Claire Le Goues <claire.legoues@gmail.com>
2010-05-25 20:38:15 +0000

    added probability for predicates at states to python script

--- datapredict/fix_trial.py
+++ datapredict/fix_trial.py
@@ -9,6 +9,46 @@ numF = 0
 def sort_dict(cbi_dict):
     return sorted(cbi_dict.items(), key= lambda t: t[1]["importance"])
 
+def do_probs(obs_dict):
+    prob_dict = {}
+
+    for predicate in obs_dict.keys():
+        prob_true = 0.0
+        prob_false = 0.0
+        pTP = 0.0
+        pFP = 0.0
+        pTF = 0.0
+        pFF = 0.0
+
+        pred_dict = obs_dict[predicate]
+        if not prob_dict.has_key(predicate):
+            prob_dict[predicate] = {}
+
+        nPT = float(pred_dict["nPT"])
+        nFT = float(pred_dict["nFT"])
+        nPO = float(pred_dict["nPO"])
+        nFO = float(pred_dict["nFO"])
+
+        total_true = nPT + nFT
+        total_false = (nPO - nPT) + (nFO - nFT)
+        total_obs = nPO + nFO
+
+        if total_obs > 0:
+            prob_true = total_true / total_obs
+            prob_false = total_false / total_obs
+        
+        if nPO > 0:
+            pTP = nPT / nPO
+        
+        pFP = 1.0 - pTP
+
+        if nFO > 0:
+            pTF = nFT / nFO
+
+        pFF = 1.0 - pTF
+        prob_dict[predicate] = {"pT" : prob_true, "pF" : prob_false, "pTP" : pTP, "pFP" : pFP, "pTF" : pTF, "pFF" : pFF}
+    return prob_dict
+
 def do_cbi(obs_dict):
     cbi_dict = {}
 
@@ -16,12 +56,12 @@ def do_cbi(obs_dict):
         pred_dict = obs_dict[predicate]
         if not cbi_dict.has_key(predicate):
             cbi_dict[predicate] = {}
-            
+
         fp = float(pred_dict["fp"])
         sp = float(pred_dict["sp"])
         fobs = float(pred_dict["fobs"])
         sobs = float(pred_dict["sobs"])
-        
+
         if fp + sp > 0:
             failurep = fp / (fp + sp)
         else:
@@ -65,13 +105,12 @@ def compute_observations(run_dict, passing_runs_list):
 
             num_obs = 0
 
+            total_num_obs = 0
             for predicate in pred_dict.keys():
                 if not obs_dict.has_key(predicate):
-                    obs_dict[predicate] = {"fp" : 0, "sp" : 0, "fobs" : 0, "sobs" : 0}
+                    obs_dict[predicate] = {"fp" : 0, "sp" : 0, "fobs" : 0, "sobs" : 0, "nPT" : 0, "nFT": 0, "nPO" : 0, "nFO" : 0}
 
                 pred_list.append(predicate)
-                fp = 0
-                sp = 0
 
                 # was it observed on this run?
                 val = 0
@@ -79,21 +118,23 @@ def compute_observations(run_dict, passing_runs_list):
                     val = 1
 
                 num_obs += val
-                fp = obs_dict[predicate]["fp"]
-                sp = obs_dict[predicate]["sp"]
+                total_num_obs += pred_dict[predicate]
 
                 if run in passing_runs_list:
-                    sp += val
+                    obs_dict[predicate]["sp"] += val
+                    obs_dict[predicate]["nPT"] += pred_dict[predicate]
                 else:
-                    fp += val
-                obs_dict[predicate]["fp"] = fp
-                obs_dict[predicate]["sp"] = sp
+                    obs_dict[predicate]["fp"] += val
+                    obs_dict[predicate]["nFT"] += pred_dict[predicate]
+
 
             for predicate in pred_list:
                 if run in passing_runs_list:
                     obs_dict[predicate]["sobs"] += num_obs
+                    obs_dict[predicate]["nPO"] += total_num_obs
                 else:
                     obs_dict[predicate]["fobs"] += num_obs
+                    obs_dict[predicate]["nFO"] += total_num_obs
 
     return obs_dict
 
@@ -134,16 +175,22 @@ def main():
     
     obs_dict = compute_observations(run_dict, passing_runs_list)
     cbi_dict = do_cbi(obs_dict)
+    prob_dict = do_probs(obs_dict)
     sorted_keys = sort_dict(cbi_dict)
 
-    print "cbi_dict:"
-    print cbi_dict
     for predicate in sorted_keys:
         print "P: " + predicate[0] + ": "
-        print "     failurep -> " + str(predicate[1]["failurep"])
-        print "     context -> " + str(predicate[1]["context"])
-        print "     increase -> " + str(predicate[1]["increase"])
-        print "     importance -> " + str(predicate[1]["importance"])
+        print "    probability T -> " + str(prob_dict[predicate[0]]["pT"])
+        print "    probability F -> " + str(prob_dict[predicate[0]]["pF"])
+        print "    probability T on passing runs -> " + str(prob_dict[predicate[0]]["pTP"])
+        print "    probability F on passing runs -> " + str(prob_dict[predicate[0]]["pFP"])
+        print "    probability T on failing runs -> " + str(prob_dict[predicate[0]]["pTF"])
+        print "    probability F on failing runs -> " + str(prob_dict[predicate[0]]["pFF"])
+        print "    failurep -> " + str(predicate[1]["failurep"])
+        print "    context -> " + str(predicate[1]["context"])
+        print "    increase -> " + str(predicate[1]["increase"])
+        print "    importance -> " + str(predicate[1]["importance"])
+
 
 
 main()
