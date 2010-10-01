9bad5501f98c539c96c4473942c97c65f4e620a3
Wes Weimer <weimer@cs.virginia.edu>
2010-10-01 22:06:19 +0000

    real-valued fitness

--- fitness.ml
+++ fitness.ml
@@ -13,9 +13,11 @@ open Rep
 open Pervasives
 
 let negative_test_weight = ref 2.0 
+let single_fitness = ref false
 let _ = 
   options := !options @ [
   "--negative_test_weight", Arg.Set_float negative_test_weight, "X negative tests fitness factor";
+  "--single-fitness", Arg.Set single_fitness, " use a single fitness value"
 ] 
 
 
@@ -31,55 +33,62 @@ exception Test_Failed
 (* As an optimization, brute force gives up on a variant as soon
  * as that variant fails a test case. *) 
 let test_to_first_failure (rep : 'a Rep.representation) = 
-  let count = ref 0 in 
+  let count = ref 0.0 in 
   try
-    for i = 1 to !neg_tests do
-      if not (rep#test_case (Negative i)) then raise Test_Failed
-      else incr count 
-    done ;
-    for i = 1 to !pos_tests do
-      if not (rep#test_case (Positive i)) then raise Test_Failed
-      else incr count 
-    done ;
-    note_success rep 
+    if !single_fitness then begin
+      let res, real_value = rep#test_case (Single_Fitness) in 
+      count := real_value ;
+      if not res then raise Test_Failed
+      else note_success rep 
+
+    end else begin 
+      for i = 1 to !neg_tests do
+        let res, v = rep#test_case (Negative i) in 
+        if not res then raise Test_Failed
+        else count := !count +. v
+      done ;
+      for i = 1 to !pos_tests do
+        let res, v = rep#test_case (Positive i) in 
+        if not res then raise Test_Failed
+        else count := !count +. v
+      done ;
+      note_success rep 
+    end 
 
   with Test_Failed -> 
-    debug "\t%3d %s\n" !count  (rep#name ()) 
+    debug "\t%3g %s\n" !count  (rep#name ()) 
 
 (* Our default fitness evaluation involves testing a variant on
  * all available test cases. *) 
 let test_all_fitness (rep : 'a representation ) = 
-  (* Find the relative weight of positive and negative tests *)
-  let fac = (float !pos_tests) *. !negative_test_weight /. 
-            (float !neg_tests) in 
   let fitness = ref 0.0 in 
   let failed = ref false in
-  for i = 1 to !pos_tests do
-    if (rep#test_case (Positive i)) then fitness := !fitness +. 1.0 
-    else failed := true 
-  done ;
-  for i = 1 to !neg_tests do
-    if (rep#test_case (Negative i)) then fitness := !fitness +. fac
-    else failed := true 
-  done ;
+
+  if !single_fitness then begin
+    (* call just a single test that will return a real value *) 
+    let res, real_value = rep#test_case (Single_Fitness) in 
+    fitness := real_value ;
+    failed := not res ; 
+
+  end else begin 
+    (* Find the relative weight of positive and negative tests *)
+    let fac = (float !pos_tests) *. !negative_test_weight /. 
+              (float !neg_tests) in 
+    for i = 1 to !pos_tests do
+      let res, _ = rep#test_case (Positive i) in 
+      if res then fitness := !fitness +. 1.0 
+      else failed := true 
+    done ;
+    for i = 1 to !neg_tests do
+      let res, _ = rep#test_case (Negative i) in 
+      if res then fitness := !fitness +. fac
+      else failed := true 
+    done ;
+  end ;
+
+  (* debugging information, etc. *) 
   debug "\t%3g %s\n" !fitness (rep#name ()) ;
   if not !failed then begin
     note_success rep 
   end ; 
   !fitness 
-
-(* ZAK - added this *)
-(* Call an outside script and read in the resulting textfile *) 
-let test_fitness_coverity (rep : 'a Rep.representation) (script_name : string) =
-  let fitness = ref 0.0 in 
-  let failed = ref false in
-  (* run all test cases *)
-  for i = 1 to !pos_tests do
-    if not (rep#test_case (Positive i)) then failed := true 
-  done ;
-
-  (* if any test cases failed, fitness=-1, otherwise fitness=coverity*)
-  fitness := ( match !failed with
-    |  true -> -1.0
-    |  false -> rep#run_coverity () ) ; 
-  !fitness
