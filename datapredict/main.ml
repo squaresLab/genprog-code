f691f4eafb3b639737c16b018a11a24448e0f638
Claire Le Goues <claire.legoues@gmail.com>
2010-10-05 15:38:24 +0000

    printing out command line arguments for debugging

--- datapredict/main.ml
+++ datapredict/main.ml
@@ -59,6 +59,20 @@ let main () = begin
   let handleArg str = parse_options_in_file str in
     Arg.parse (Arg.align !options) handleArg usageMsg ;
 
+	liter (fun (name,arg,_) ->
+			 pprintf "%s %s\n" name
+			   (match arg with
+				| Arg.Set br 
+				| Arg.Clear br 
+				  -> Printf.sprintf "%b" !br 
+				| Arg.Set_string sr
+				  -> Printf.sprintf "%S" !sr
+				| Arg.Set_int ir
+				  -> Printf.sprintf "%d" !ir
+				| Arg.Set_float fr
+				  -> Printf.sprintf "%g" !fr
+				| _ -> "?"); flush stdout
+		  ) (List.sort (fun (a,_,_) (a',_,_) -> compare a a') (!options)) ; 
     (* get relevant hashtables from instrumentation *)
     let max_site = ref 0 in
     let in_channel = open_in !cbi_hash_tables in 
@@ -81,66 +95,32 @@ let main () = begin
 		end;
 		Printf.printf "Debug1\n"; flush stdout;
 		let graph = DynamicExecGraph.build_graph !file_list in
-		Printf.printf "Debug2\n"; flush stdout;
-
 		  DynamicExecGraph.print_graph graph;
-		  pprintf "post print\n"; flush stdout;
-(*		  if !do_cbi then begin debug, don't bother with the flag *)
-			let ranked = DynamicPredict.invs_that_predict_inv graph (RunFailed) in
-		  pprintf "post ranked\n"; flush stdout;
-			  liter
-				(fun (p1,s1,rank1) -> 
-				   let e = 
-					 match p1 with
-					   CilExp(e) -> e
-					 | _ -> failwith "rank print not implemented"
-				   in
-				   let exp_str = Pretty.sprint 80 (d_exp () e) in
-					 pprintf "pred: %s, state: %d, f_P: %d s_P: %d, f_P_obs: %d s_P_obs: %d, failure_P: %g, context:%g,increase: %g, imp: %g\n" 
-	  				   exp_str s1 rank1.f_P rank1.s_P rank1.f_P_obs
-					   rank1.s_P_obs rank1.failure_P rank1.context rank1.increase
-					   rank1.importance; flush stdout)
-				ranked;
-			  pprintf "really post ranked\n"; flush stdout;
-			  (* we've ranked; now, if to_eval is set, evaluate that
-				 predicate at every state on every run. *)
-			  (* for now, debug: *)
-			  let pred = match (List.hd ranked) with (p1,s1,rank1) -> p1 in
-				pprintf "Propagating and predicting the top predictor: ";
-				d_pred pred; flush stdout;
-				ignore(DynamicExecGraph.propagate_predicate graph pred);
-				let ranked = DynamicPredict.invs_that_predict_inv graph (pred) in 
-				  liter
-					(fun (p1,s1,rank1) -> 
-					   let e = 
-						 match p1 with
-						   CilExp(e) -> e
-						 | _ -> failwith "rank print not implemented"
-					   in
-					   let exp_str = Pretty.sprint 80 (d_exp () e) in
-						 pprintf "pred: %s, state: %d, f_P: %d s_P: %d, f_P_obs: %d s_P_obs: %d, failure_P: %g, context:%g,increase: %g, imp: %g\n" 
-	  					   exp_str s1 rank1.f_P rank1.s_P rank1.f_P_obs
-						   rank1.s_P_obs rank1.failure_P rank1.context rank1.increase
-						   rank1.importance; flush stdout)
-					ranked;
-				  pprintf "\n\n\nPredicting success: \n";
-				  let ranked = DynamicPredict.invs_that_predict_inv graph
-					(RunSucceeded) in
-					liter
-					  (fun (p1,s1,rank1) -> 
-						 let e = 
-						   match p1 with
-							 CilExp(e) -> e
-						   | _ -> failwith "rank print not implemented"
-						 in
-						 let exp_str = Pretty.sprint 80 (d_exp () e) in
-						   pprintf "pred: %s, state: %d, f_P: %d s_P: %d, f_P_obs: %d s_P_obs: %d, failure_P: %g, context:%g,increase: %g, imp: %g\n" 
-	  						 exp_str s1 rank1.f_P rank1.s_P rank1.f_P_obs
-							 rank1.s_P_obs rank1.failure_P rank1.context rank1.increase
-							 rank1.importance; flush stdout)
-					  ranked;
-					DynamicExecGraph.print_fault_localization graph true true !inter_weights
-			
+		  (*		  if !do_cbi then begin debug, don't bother with the flag *)
+		  let ranked = DynamicPredict.invs_that_predict_inv graph (RunFailed) in
+			pprintf "post ranked\n"; flush stdout;
+			liter
+			  (fun (p1,s1,rank1) -> 
+				 let e = 
+				   match p1 with
+					 CilExp(e) -> e
+				   | _ -> failwith "rank print not implemented"
+				 in
+				 let exp_str = Pretty.sprint 80 (d_exp () e) in
+				   pprintf "pred: %s, state: %d, f_P: %d s_P: %d, f_P_obs: %d s_P_obs: %d, failure_P: %g, context:%g,increase: %g, imp: %g\n" 
+	  				 exp_str s1 rank1.f_P rank1.s_P rank1.f_P_obs
+					 rank1.s_P_obs rank1.failure_P rank1.context rank1.increase
+					 rank1.importance; flush stdout)
+			  ranked;
+			pprintf "really post ranked\n"; flush stdout;
+			let pred = match (List.hd ranked) with (p1,s1,rank1) -> p1 in
+			  pprintf "Propagating and predicting the top predictor: ";
+			  d_pred pred; flush stdout;
+			  DynamicExecGraph.propagate_predicate graph pred;
+			  let ranked1 = DynamicPredict.invs_that_predict_inv graph (pred) in 
+			  let ranked2 = DynamicPredict.invs_that_predict_inv graph in
+				DynamicExecGraph.print_fault_localization graph true true !inter_weights
+				  
 (*		  end*)
 end ;;
 
