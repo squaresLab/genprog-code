3f47d919ab4df7a86a8f8805af35f8356905ae72
Wes Weimer <weimer@cs.virginia.edu>
2010-10-20 03:00:43 +0000

    multi-objective optimization!

--- repair/multiopt-test/test.sh
+++ repair/multiopt-test/test.sh
@@ -5,16 +5,17 @@
 # $4 = source name
 # $5 = single-fitness-file name 
 # exit 0 = success
-ulimit -t 1
+ulimit -t 5
 case $2 in
   p1) $1 | diff output - && exit 0 ;;
 
   s) # fitness 
   $1 > test.tmp
-  ./difference `cat test.tmp` `cat output` > $5
-  echo ,`cat $1 | wc -c` >> $5
+  echo `cat $1 | wc -c`, > $5
+  ./difference `cat test.tmp` `cat output` >> $5
   rm test.tmp 
   exit 1 
+  ;;
 
 esac 
 exit 1
