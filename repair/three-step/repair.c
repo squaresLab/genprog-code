4956162f86bc4daf165ed2f8f9bd6eaea9b67b07
Ethan Fast <ejf3z@virginia.edu>
2010-11-05 12:13:43 +0000

    'bug we can't fix'

---
+++ repair/three-step/repair.c
@@ -0,0 +1,366 @@
+/* compiler builtin: 
+   void __builtin_varargs_start(__builtin_va_list  ) ;  */
+/* compiler builtin: 
+   int __builtin_strcmp(char const   * , char const   * ) ;  */
+/* compiler builtin: 
+   void *__builtin___memmove_chk(void * , void const   * , unsigned long  ,
+                                 unsigned long  ) ;  */
+/* compiler builtin: 
+   char *__builtin_strpbrk(char const   * , char const   * ) ;  */
+/* compiler builtin: 
+   void *__builtin_memcpy(void * , void const   * , unsigned long  ) ;  */
+/* compiler builtin: 
+   double __builtin_exp(double  ) ;  */
+/* compiler builtin: 
+   long double __builtin_nanl(char const   * ) ;  */
+/* compiler builtin: 
+   double __builtin_cos(double  ) ;  */
+/* compiler builtin: 
+   char *__builtin_strchr(char * , int  ) ;  */
+/* compiler builtin: 
+   float __builtin_atan2f(float  , float  ) ;  */
+/* compiler builtin: 
+   void *__builtin___memcpy_chk(void * , void const   * , unsigned long  ,
+                                unsigned long  ) ;  */
+/* compiler builtin: 
+   double __builtin_asin(double  ) ;  */
+/* compiler builtin: 
+   int __builtin_ctz(unsigned int  ) ;  */
+/* compiler builtin: 
+   char *__builtin_stpcpy(char * , char const   * ) ;  */
+/* compiler builtin: 
+   double __builtin_nans(char const   * ) ;  */
+/* compiler builtin: 
+   long double __builtin_atan2l(long double  , long double  ) ;  */
+/* compiler builtin: 
+   float __builtin_logf(float  ) ;  */
+/* compiler builtin: 
+   int __builtin___fprintf_chk(void * , int  , char const   *  , ...) ;  */
+/* compiler builtin: 
+   int __builtin___vsprintf_chk(char * , int  , unsigned long  ,
+                                char const   * , __builtin_va_list  ) ;  */
+/* compiler builtin: 
+   char *__builtin___strncpy_chk(char * , char const   * , unsigned long  ,
+                                 unsigned long  ) ;  */
+/* compiler builtin: 
+   float __builtin_log10f(float  ) ;  */
+/* compiler builtin: 
+   double __builtin_atan(double  ) ;  */
+/* compiler builtin: 
+   void *__builtin_alloca(unsigned long  ) ;  */
+/* compiler builtin: 
+   void __builtin_va_end(__builtin_va_list  ) ;  */
+/* compiler builtin: 
+   int __builtin_strncmp(char const   * , char const   * , unsigned long  ) ;  */
+/* compiler builtin: 
+   double __builtin_sin(double  ) ;  */
+/* compiler builtin: 
+   long double __builtin_logl(long double  ) ;  */
+/* compiler builtin: 
+   float __builtin_coshf(float  ) ;  */
+/* compiler builtin: 
+   void *__builtin___mempcpy_chk(void * , void const   * , unsigned long  ,
+                                 unsigned long  ) ;  */
+/* compiler builtin: 
+   char *__builtin___strcat_chk(char * , char const   * , unsigned long  ) ;  */
+/* compiler builtin: 
+   float __builtin_nansf(char const   * ) ;  */
+/* compiler builtin: 
+   void *__builtin_memset(void * , int  , int  ) ;  */
+/* compiler builtin: 
+   void __builtin_va_copy(__builtin_va_list  , __builtin_va_list  ) ;  */
+/* compiler builtin: 
+   float __builtin_sinhf(float  ) ;  */
+/* compiler builtin: 
+   long double __builtin_log10l(long double  ) ;  */
+/* compiler builtin: 
+   long double __builtin_coshl(long double  ) ;  */
+/* compiler builtin: 
+   int __builtin_ffs(unsigned int  ) ;  */
+/* compiler builtin: 
+   float __builtin_asinf(float  ) ;  */
+/* compiler builtin: 
+   long double __builtin_nansl(char const   * ) ;  */
+/* compiler builtin: 
+   double __builtin_frexp(double  , int * ) ;  */
+/* compiler builtin: 
+   double __builtin_tan(double  ) ;  */
+/* compiler builtin: 
+   long double __builtin_sinhl(long double  ) ;  */
+/* compiler builtin: 
+   float __builtin_frexpf(float  , int * ) ;  */
+/* compiler builtin: 
+   long double __builtin_asinl(long double  ) ;  */
+/* compiler builtin: 
+   void *__builtin_frame_address(unsigned int  ) ;  */
+/* compiler builtin: 
+   double __builtin_floor(double  ) ;  */
+/* compiler builtin: 
+   float __builtin_tanhf(float  ) ;  */
+/* compiler builtin: 
+   int __builtin_parityl(unsigned long  ) ;  */
+/* compiler builtin: 
+   int __builtin_clzl(unsigned long  ) ;  */
+/* compiler builtin: 
+   double __builtin_powi(double  , int  ) ;  */
+/* compiler builtin: 
+   long double __builtin_frexpl(long double  , int * ) ;  */
+/* compiler builtin: 
+   float __builtin_atanf(float  ) ;  */
+/* compiler builtin: 
+   float __builtin_huge_valf(void) ;  */
+/* compiler builtin: 
+   float __builtin_sqrtf(float  ) ;  */
+/* compiler builtin: 
+   float __builtin_fmodf(float  ) ;  */
+/* compiler builtin: 
+   unsigned long __builtin_object_size(void * , int  ) ;  */
+/* compiler builtin: 
+   void __builtin_va_arg(__builtin_va_list  , unsigned long  , void * ) ;  */
+/* compiler builtin: 
+   void __builtin_stdarg_start(__builtin_va_list  ) ;  */
+/* compiler builtin: 
+   long double __builtin_tanhl(long double  ) ;  */
+/* compiler builtin: 
+   double __builtin_nan(char const   * ) ;  */
+/* compiler builtin: 
+   void __builtin_return(void const   * ) ;  */
+/* compiler builtin: 
+   long double __builtin_atanl(long double  ) ;  */
+/* compiler builtin: 
+   long double __builtin_huge_vall(void) ;  */
+/* compiler builtin: 
+   float __builtin_inff(void) ;  */
+/* compiler builtin: 
+   long double __builtin_sqrtl(long double  ) ;  */
+/* compiler builtin: 
+   long double __builtin_fmodl(long double  ) ;  */
+/* compiler builtin: 
+   int __builtin___printf_chk(int  , char const   *  , ...) ;  */
+/* compiler builtin: 
+   float __builtin_floorf(float  ) ;  */
+/* compiler builtin: 
+   float __builtin_fabsf(float  ) ;  */
+/* compiler builtin: 
+   int __builtin_popcountll(unsigned long long  ) ;  */
+/* compiler builtin: 
+   int __builtin___sprintf_chk(char * , int  , unsigned long  , char const   * 
+                               , ...) ;  */
+/* compiler builtin: 
+   int __builtin___vprintf_chk(int  , char const   * , __builtin_va_list  ) ;  */
+/* compiler builtin: 
+   int __builtin___snprintf_chk(char * , unsigned long  , int  ,
+                                unsigned long  , char const   *  , ...) ;  */
+/* compiler builtin: 
+   long double __builtin_infl(void) ;  */
+/* compiler builtin: 
+   void *__builtin_mempcpy(void * , void const   * , unsigned long  ) ;  */
+/* compiler builtin: 
+   long double __builtin_floorl(long double  ) ;  */
+/* compiler builtin: 
+   int __builtin_ctzl(unsigned long  ) ;  */
+/* compiler builtin: 
+   long double __builtin_fabsl(long double  ) ;  */
+/* compiler builtin: 
+   int __builtin_clz(unsigned int  ) ;  */
+/* compiler builtin: 
+   double __builtin_fabs(double  ) ;  */
+/* compiler builtin: 
+   int __builtin_popcount(unsigned int  ) ;  */
+/* compiler builtin: 
+   double __builtin_ceil(double  ) ;  */
+/* compiler builtin: 
+   double __builtin_ldexp(double  , int  ) ;  */
+/* compiler builtin: 
+   float __builtin_sinf(float  ) ;  */
+/* compiler builtin: 
+   float __builtin_acosf(float  ) ;  */
+/* compiler builtin: 
+   int __builtin___vsnprintf_chk(char * , unsigned long  , int  ,
+                                 unsigned long  , char const   * ,
+                                 __builtin_va_list  ) ;  */
+/* compiler builtin: 
+   double __builtin_sinh(double  ) ;  */
+/* compiler builtin: 
+   int __builtin_ffsll(unsigned long long  ) ;  */
+/* compiler builtin: 
+   char *__builtin___strcpy_chk(char * , char const   * , unsigned long  ) ;  */
+/* compiler builtin: 
+   double __builtin_inf(void) ;  */
+/* compiler builtin: 
+   void __builtin_prefetch(void const   *  , ...) ;  */
+/* compiler builtin: 
+   long double __builtin_sinl(long double  ) ;  */
+/* compiler builtin: 
+   long double __builtin_acosl(long double  ) ;  */
+/* compiler builtin: 
+   double __builtin_sqrt(double  ) ;  */
+/* compiler builtin: 
+   double __builtin_fmod(double  ) ;  */
+/* compiler builtin: 
+   char *__builtin_strcpy(char * , char const   * ) ;  */
+/* compiler builtin: 
+   float __builtin_ceilf(float  ) ;  */
+/* compiler builtin: 
+   void *__builtin_return_address(unsigned int  ) ;  */
+/* compiler builtin: 
+   char *__builtin___stpcpy_chk(char * , char const   * , unsigned long  ) ;  */
+/* compiler builtin: 
+   float __builtin_tanf(float  ) ;  */
+/* compiler builtin: 
+   int __builtin_parityll(unsigned long long  ) ;  */
+/* compiler builtin: 
+   float __builtin_ldexpf(float  , int  ) ;  */
+/* compiler builtin: 
+   int __builtin_types_compatible_p(unsigned long  , unsigned long  ) ;  */
+/* compiler builtin: 
+   double __builtin_log10(double  ) ;  */
+/* compiler builtin: 
+   float __builtin_expf(float  ) ;  */
+/* compiler builtin: 
+   int __builtin_clzll(unsigned long long  ) ;  */
+/* compiler builtin: 
+   double __builtin_tanh(double  ) ;  */
+/* compiler builtin: 
+   int __builtin_constant_p(int  ) ;  */
+/* compiler builtin: 
+   long double __builtin_ceill(long double  ) ;  */
+/* compiler builtin: 
+   long double __builtin_tanl(long double  ) ;  */
+/* compiler builtin: 
+   double __builtin_log(double  ) ;  */
+/* compiler builtin: 
+   long double __builtin_ldexpl(long double  , int  ) ;  */
+/* compiler builtin: 
+   long double __builtin_expl(long double  ) ;  */
+/* compiler builtin: 
+   int __builtin_popcountl(unsigned long  ) ;  */
+/* compiler builtin: 
+   void *__builtin___memset_chk(void * , int  , unsigned long  , unsigned long  ) ;  */
+/* compiler builtin: 
+   char *__builtin___strncat_chk(char * , char const   * , unsigned long  ,
+                                 unsigned long  ) ;  */
+/* compiler builtin: 
+   double __builtin_huge_val(void) ;  */
+/* compiler builtin: 
+   __builtin_va_list __builtin_next_arg(void) ;  */
+/* compiler builtin: 
+   float __builtin_powif(float  , int  ) ;  */
+/* compiler builtin: 
+   int __builtin___vfprintf_chk(void * , int  , char const   * ,
+                                __builtin_va_list  ) ;  */
+/* compiler builtin: 
+   float __builtin_modff(float  , float * ) ;  */
+/* compiler builtin: 
+   double __builtin_atan2(double  , double  ) ;  */
+/* compiler builtin: 
+   char *__builtin_strncpy(char * , char const   * , unsigned long  ) ;  */
+/* compiler builtin: 
+   long double __builtin_powil(long double  , int  ) ;  */
+/* compiler builtin: 
+   float __builtin_cosf(float  ) ;  */
+/* compiler builtin: 
+   unsigned long __builtin_strspn(char const   * , char const   * ) ;  */
+/* compiler builtin: 
+   long double __builtin_modfl(long double  , long double * ) ;  */
+/* compiler builtin: 
+   int __builtin_parity(unsigned int  ) ;  */
+/* compiler builtin: 
+   double __builtin_cosh(double  ) ;  */
+/* compiler builtin: 
+   char *__builtin_strncat(char * , char const   * , unsigned long  ) ;  */
+/* compiler builtin: 
+   long __builtin_expect(long  , long  ) ;  */
+/* compiler builtin: 
+   double __builtin_acos(double  ) ;  */
+/* compiler builtin: 
+   long double __builtin_cosl(long double  ) ;  */
+/* compiler builtin: 
+   void __builtin_va_start(__builtin_va_list  ) ;  */
+/* compiler builtin: 
+   int __builtin_ctzll(unsigned long long  ) ;  */
+/* compiler builtin: 
+   unsigned long __builtin_strcspn(char const   * , char const   * ) ;  */
+/* compiler builtin: 
+   int __builtin_ffsl(unsigned long  ) ;  */
+/* compiler builtin: 
+   float __builtin_nanf(char const   * ) ;  */
+#line 2
+extern int ( /* missing proto */  atoi)() ;
+#line 11
+extern int ( /* missing proto */  printf)() ;
+#line 1 "three-step.c"
+int main(int argv , char **argc ) 
+{ int x ;
+  int tmp ;
+  int p1 ;
+  int p2 ;
+  int p3 ;
+  int now ;
+
+  {
+#line 2
+  tmp = atoi(*(argc + 1));
+#line 2
+  x = tmp;
+#line 3
+  p1 = 0;
+#line 4
+  p2 = 0;
+#line 5
+  p3 = 0;
+#line 9
+  now = (p1 - p2) - p3;
+#line 10
+  if (x == 1) {
+#line 11
+    printf("%d:%d:%d\n", x, (p1 - p2) - p3, now == (p1 - p2) - p3);
+  } else {
+
+  }
+#line 13
+  if (x == 2) {
+#line 14
+    printf("%d:%d:%d\n", x, (p1 - p2) - p3, now == (p1 - p2) - p3);
+  } else {
+
+  }
+#line 16
+  if (x == 3) {
+#line 17
+    printf("%d:%d:%d\n", x, (p1 - p2) - p3, now == (p1 - p2) - p3);
+  } else {
+
+  }
+#line 19
+  if (x == 4) {
+#line 20
+    printf("%d:%d:%d\n", x, (p1 - p2) - p3, now == (p1 - p2) - p3);
+  } else {
+
+  }
+#line 22
+  if (x == 5) {
+#line 23
+    printf("%d:%d:%d\n", x, (p1 - p2) - p3, now == (p1 - p2) - p3);
+  } else {
+
+  }
+#line 30
+  p3 = 4;
+#line 28
+  p1 = 7;
+#line 29
+  p2 = 3;
+#line 25
+  if (x == 666) {
+#line 26
+    printf("%d:%d:%d:%d\n", x, (p1 + p2) + p3, (p1 - p2) - p3,
+           now == (p1 - p2) - p3);
+  } else {
+
+  }
+#line 31
+  return (0);
+}
+}
