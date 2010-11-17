1f264140cce6fd1e6a070d70cb630eb40a0f89c6
Claire Le Goues <claire.legoues@gmail.com>
2010-11-17 21:24:20 +0000

    implemented k-medoid clustering, and tested on simple x-y points

--- taxonomy/diffs.ml
+++ taxonomy/diffs.ml
@@ -1,5 +1,6 @@
 open Batteries 
 open Utils
+open Ref
 open Unix
 open IO
 open Enum
@@ -8,33 +9,77 @@ open String
 open List
 open Globals
 
-let rev_num_regexp = Str.regexp "^r[0-9]+"
-let space_regexp = Str.regexp_string " "
-let dashes_regexp = Str.regexp_string "------------------------------------------------------------------------"
-let fix_regexp = Str.regexp_string_case_fold "fix"
-let index_regexp = Str.regexp_string "Index: "
-let eqs =  Str.regexp_string "==================================================================="
-let pluses = Str.regexp_string "+++"
-let minuses = Str.regexp_string "---"
+module type DataPoint = 
+sig
+
+  type t
+
+  val to_string : t -> string
+  val compare : t -> t -> int
+  val cost : t -> t -> float
+  val distance : t -> t -> float
+end
+
+module type Diffs =
+sig
+  type t
+	
+  (* Takes the url of an svn repository and a start and end revision
+   * number, returns an enumeration of diffs that have to do with
+   * "fixes", as specified in the log message *)
+
+  val get_diffs : string -> int -> int -> t Enum.t
+
+  (* cost should be normalized, and should involve caching. *)
+  val cost : t -> t -> float
+
+  val compare : t -> t -> int
+end
+
+module XYPoint =
+struct 
+  
+  type t =
+	  { x : int ;
+		y : int ; }
+
+  let to_string p = Printf.sprintf "%d,%d\n" p.x p.y
+	
+  let create x y = { x=x;y=y;}
+
+  let compare p1 p2 = ((p1.x - p2.x) * (p1.x - p2.x)) + ((p1.y - p2.y) * (p1.y - p2.y))
+  let cost p1 p2 = float_of_int ((abs (p1.x - p2.x)) + (abs (p1.y - p2.y)))
+  let distance p1 p2 =  sqrt (float_of_int (compare p1 p2))
+end
 
 module Diffs =
 struct
 
+  (* this record type is "private" *)
   type rev = {
 	revnum : int;
 	logmsg : string;
 	files : (string * string list) Enum.t;
   }
 
+  (* diff type and initialization *)
+  let diffid = ref 0
+
   type diff = {
 	id : int;
-	rinfo : int;
+	rev_num : int;
 	fname : string;
 	msg : string;
 	syntactic : string list;
   }
 
-  let collect_diffs rev url =
+  let new_diff revnum fname msg syntactic = 
+	{id=(post_incr diffid);rev_num=revnum;fname=fname;msg=msg;syntactic=syntactic}
+
+
+  (* collect diffs is a helper function for get_diffs *)
+
+  let collect_diffs (rev : rev) (url : string) : diff Enum.t =
 	let diffcmd = "svn diff -r"^(of_int (rev.revnum-1))^":"^(of_int rev.revnum)^" "^url in
 	let innerInput = open_process_in ?autoclose:(Some(true)) ?cleanup:(Some(true)) diffcmd in
 	let enumInput =  IO.lines_of innerInput in
@@ -60,9 +105,7 @@ struct
 			   end 
 			 else 
 			   if (String.is_empty fname) ||
-				 (string_match eqs str 0) ||
-				 (string_match pluses str 0) ||
-				 (string_match minuses str 0) then
+				 (string_match junk str 0) then
 				   (finfos,(fname,strs))
 			   else (finfos,(fname,str::strs))
 		) ([],("",[])) enumInput
@@ -71,11 +114,10 @@ struct
 	let files = efilt (fun (str,_) -> not (String.is_empty str)) finfos in
 	  ignore(close_process_in innerInput);
 	  emap 
-		(fun (str,diff) ->
-		   {id=0;rinfo=rev.revnum;fname=str;msg=rev.logmsg;syntactic=(List.rev diff)}
+		(fun (str,diff) -> new_diff rev.revnum str rev.logmsg (List.rev diff)
 		) files 
 
-  let get_revs url startrev endrev =
+  let get_diffs url startrev endrev =
 	let logcmd = "svn log "^url^" -r"^(of_int startrev)^":"^(of_int endrev) in
 	let proc = open_process_in ?autoclose:(Some(true)) ?cleanup:(Some(true)) logcmd in
 	let log =  IO.lines_of proc in
@@ -105,13 +147,12 @@ struct
 	let with_files = 
 	  eflat (emap (fun rev -> collect_diffs rev url) only_fixes) 
 	in
-	let finale = efilt (fun diff -> not (String.is_empty diff.fname)) with_files in
-	  pprintf "before final iter\n"; flush stdout;
-	  eiter
-		(fun diff ->
-		   pprintf "Diff id: %d; revnum: %d; fname: %s, msg: %s, syntactic: "
-			 diff.id diff.rinfo diff.fname diff.msg;
-		   liter (fun str -> pprintf "%s\n" str) diff.syntactic; flush stdout
-		) finale
+	  efilt (fun diff -> not (String.is_empty diff.fname)) with_files (*in*)
+		
+  (* this takes integers as diff keys instead of proper diffs, which I
+	 think should be faster/take less memory? Maybe not *)
+
+  let distance d1 d2 = failwith "Not implemented" (* REMEMBER TO CACHE! *)
+	
 
 end
--- taxonomy/main.ml
+++ taxonomy/main.ml
@@ -8,12 +8,15 @@
  *)
 
 open Batteries
+open List
 open Utils
 open Diffs
+open Cluster
 
 let repos = ref ""
 let rstart = ref 0
 let rend = ref 0
+let xy_data = ref ""
 
 let usageMsg = "Fix taxonomy clustering.  Right now assumes svn repository.\n"
 
@@ -21,14 +24,27 @@ let options = [
   "--repos", Arg.Set_string repos, "\t URL of the repository.";
   "--rstart", Arg.Set_int rstart, "\t Start revision.  Default: 0.";
   "--rend", Arg.Set_int rend, "\t End revision.  Default: latest.";
+  "--test-cluster", Arg.Set_string xy_data, "\t Test data of XY points to test the clustering"
 ]
 
 
 let main () = begin
+  Random.init (Random.bits ());
   handle_options options usageMsg;
-  ignore(Diffs.get_revs !repos !rstart !rend)
-(*	Diffs.get_diffs !repos fix_revs*)
-	
+  if !xy_data <> "" then begin
+	let lines = File.lines_of !xy_data in
+	let points = 
+	  Set.of_enum 
+		(Enum.map 
+		   (fun line -> 
+			  let split = Str.split comma_regexp line in
+			  let x,y = (int_of_string (hd split)), (int_of_string (hd (tl split))) in
+				XYPoint.create x y 
+		   ) lines)
+	in
+	  pprintf "made data set\n"; flush stdout;
+	  ignore(TestCluster.kmedoid 2 points)
+  end else ignore(Diffs.get_diffs !repos !rstart !rend)
 
 end ;;
 
