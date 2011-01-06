ee614bb377910ae52a55194c210c02e3c2fe745c
Claire Le Goues <claire.legoues@gmail.com>
2011-01-06 23:46:20 +0000

    made a bunch of changes, checkpoint

--- taxonomy/cabs/glr.ml
+++ taxonomy/cabs/glr.ml
@@ -18,7 +18,7 @@ open Smutil          (* getSome, etc. *)
 
 
 (* when true, print parse actions *)
-let traceParse:bool = false
+let traceParse:bool = true
 
 (* when true, keep some statistics useful for performance evaluation *)
 let accounting:bool = false
--- taxonomy/cabs/main.ml
+++ taxonomy/cabs/main.ml
@@ -15,9 +15,11 @@ open Cprint
 
 let usageMsg = "A test driver for C and C-diff parsing"
 let parse_type = ref "c"
+let long = ref false
 
 let options =  [
   "-type", Arg.Set_string parse_type, "\t Diff or C parsing? Default: .c";
+  "-long", Arg.Set long, "\t a long file as printed by diffs"
 ] 
 
 let main () = 
@@ -31,13 +33,42 @@ let main () =
 		 | "c" -> let ast = Cparse.parse_file filename in 
 			 Printf.printf "Done parsing; about to print\n"; flush stdout;
 			 dumpFile defaultCabsPrinter stdout (filename,ast)
-		 | "diff" -> let ast,count = Diffparse.parse_file filename in
-			 Printf.printf "Done parsing; about to print\n"; flush stdout;
-			 dumpTree defaultCabsPrinter stdout (filename,ast)
+		 | "diff" -> 
+			 if not !long then begin
+			   let ast,count = Diffparse.parse_file filename in
+				 Printf.printf "Done parsing; about to print\n"; flush stdout;
+				 dumpTree defaultCabsPrinter stdout (filename,ast)
+			 end else begin
+			   Printf.printf "Here\n"; flush stdout;
+			   let num_succeed,num_fail = ref 0, ref 0 in
+			   let reg = Str.regexp_string "SEPSEPSEPSEP" in
+			   let fin = open_in filename in 
+				 try
+				   let current_snippet = ref "" in
+					 while true do
+					   let str = input_line fin in 
+						 if Str.string_match reg str 0 then begin
+						   Printf.printf "Current snippet: %s\n" !current_snippet; flush stdout;
+						   (try
+							  let ast,count = Diffparse.parse_from_string !current_snippet in 
+								Printf.printf "Done parsing; about to print\n"; flush stdout;
+								dumpTree defaultCabsPrinter stdout (filename,ast);
+								incr num_succeed; Printf.printf "Succeeded; %d successes so far\n" !num_succeed; flush stdout
+							with _ -> 
+							  begin 
+								incr num_fail; Printf.printf "Failed to parse. %d failures so far\n" !num_fail; flush stdout
+							  end);
+						   Printf.printf "Resetting current snippet\n"; flush stdout;
+						   current_snippet := ""
+						 end else
+						   current_snippet := !current_snippet ^ "\n" ^ str
+					 done
+				 with End_of_file -> ()
+			 end
 		 | _ -> 
 			 let s = Printf.sprintf "Unrecognized file type to parse: %s\n" (String.lowercase !parse_type) in
 			   failwith s) !files_to_parse;
-    Printf.printf "Done!"; flush stdout
+    Printf.printf "Done!\n\n"; flush stdout
 ;;
 
 main () ;;
