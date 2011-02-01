28f75901954d7bfc62c94e29be3e1304e8d85719
Ethan Fast <ejf3z@virginia.edu>
2011-02-01 19:58:27 +0000

    remove poorly located dirs

--- repair/bzip2/checkrobust.rb
+++
@@ -1,7 +0,0 @@
-rbfiles = Dir.glob("robus*.c")
-rbfiles.map{|x| x.split(".")[0]}.each do |x|
-	`gcc -O2 -S #{x}.c`
-	puts x
-	`diff repair.s #{x}.s > #{x}.diff`
-	`wc -l #{x}.diff > #{x}.count`
-end
--- repair/vyquon/checkrobust.rb
+++
@@ -1,6 +0,0 @@
-rbfiles = Dir.glob("robus*.c")
-rbfiles.map{|x| x.split(".")[0]}.each do |x|
-	`gcc -O2 -S #{x}.c`
-	`diff repair.s #{x}.s > #{x}.diff`
-	`wc -l #{x}.diff > #{x}.count`
-end
