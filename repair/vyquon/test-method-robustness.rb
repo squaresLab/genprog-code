00f02e993b4693b288e91f1ac03182d93e1f9b4a
Ethan Fast <ejf3z@virginia.edu>
2011-02-01 19:58:27 +0000

    remove poorly located dirs

--- repair/vyquon/test-method-robustness.rb
+++
@@ -1,7 +0,0 @@
-methods = File.new('methods','r').to_a
-methods.map{|x| x.to_s.chomp}.each do |method|
-	puts "Running #{method}"
-	`rm *.cache`
-	puts "Removed cache"
-	`../repair run-robust --mutate-func #{method} > #{method}`
-end
--- repair/vyquon/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-ulimit -t 2
-rm -rf my.tp1 my.tp2 my.tp3 my.tp4 my.tp5
-case $2 in
-				p1) $1 test/test.vy | grep "Unnamed" - | wc -l | diff pass - && exit 0 ;;
-				n1) $1 test/test.vy && exit 1 ;;
-esac
-exit 1
