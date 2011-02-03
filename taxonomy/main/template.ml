31ac1bfcb30ccae74277c0e981771c35a6f2c31e
Claire Le Goues <claire.legoues@gmail.com>
2011-02-03 21:32:20 +0000

    about to refactor

--- taxonomy/main/template.ml
+++ taxonomy/main/template.ml
@@ -68,7 +68,11 @@ let print_template (con,changes) =
 (* a template is one change to one location in code, meaning a treediff converts
    into a list of templates *)
 
-(* FIXME: add to utils *)
+let tfold (ts : template list) (cons : context) (fn : template list -> context -> 'a -> template list) =
+  lfoldl
+	(fun templates ->
+	  fun ele -> fn templates cons ele) ts
+		
 let make_dum_def dlist = lmap (fun d -> DEF(d)) dlist
 let make_dum_stmt slist = lmap (fun s -> STMT(s)) slist
 let make_dum_exp elist = lmap (fun e -> EXP(e)) elist
@@ -95,157 +99,250 @@ let treediff_to_templates (tree1 : tree) (difftree1 : diff_tree_node) (tdiff : c
 	| _ -> failwith "Why are we inserting nowhere, anyway?"
   in
 	liter organized_changes tdiff;
-	pprintf "after organized changes\n"; flush stdout;
-	let rec walk_treenode tn arounds guarded_by templates =
+	let rec walk_treenode templates initial_context tn =
 	  let diff_tree_node = hfind cabs_id_to_diff_tree_node tn.id in
-		pprintf "walk_treenode tl: %s\n" diff_tree_node.tl_str; flush stdout;
 	  let dummy_node = diff_tree_node.original_node in
 	  let tn_p = 
 		match dummy_node with 
 		  TREENODE(tn) -> Some(tn)
 		| _ -> failwith "Expected a treenode dummy type, got something else"
 	  in
+	  let initial_context = {initial_context with parent_treenode = tn_p} in
 	  let templates = 
 		if hmem context_ht diff_tree_node.nid then
-		  ((make_context tn_p None None None arounds guarded_by (Set.empty)),hfind context_ht diff_tree_node.nid)::templates
+		  (initial_context,hfind context_ht diff_tree_node.nid)::templates
 		else templates
 	  in
 		match tn.node with
-		| Globals(dlist) ->
-		  lfoldl
-			(fun templates -> 
-			  fun def ->
-				walk_def def tn_p None None (Set.union (Set.of_enum (List.enum (make_dum_def dlist))) arounds) guarded_by (Set.empty) templates)
-			templates dlist
-		| Stmts(slist) -> walk_stmt_list slist tn_p None None arounds guarded_by (Set.empty)  templates
-		| Exps(elist) ->
-		  lfoldl 
-			(fun templates -> 
-			  fun exp ->
-				walk_exp exp tn_p None None (Set.union (Set.of_enum (List.enum (make_dum_exp elist))) arounds) [] (Set.empty) templates)
-			templates elist
+		| Globals(dlist) -> walk_defs templates initial_context dlist
+		| Stmts(slist) -> walk_stmts templates initial_context slist 
+		| Exps(elist) -> walk_exps templates initial_context elist
 		| _ -> failwith "I really should get rid of the syntax tree node type since I don't use it."
-
-	and walk_stmt_list slist tn_p def_p exp_p arounds guarded_by guarding templates =
-	  pprintf "in walk_stmt_list\n"; flush stdout;
-	  let arounds = Set.union arounds (Set.of_enum (List.enum (make_dum_stmt slist))) in
+	and wattr templates context (str,elist) =
+	  walk_exps templates context elist
+	and wattrs templates context attrs = 
+	  tfold templates context wattr attrs
+	and wname templates context (str,dt,attrs,_) = 
+	  let templates' = wdt templates context dt in
+		wattrs templates' context attrs
+	and wfg templates context (spec, lst) = 
+	  let templates' = wspecs templates context spec in
+		lfoldl
+		  (fun templates ->
+			fun (name,eno) ->
+			  let templates' = wname templates context name in 
+				match eno with
+				  None -> templates'
+				| Some(e) -> walk_exp templates' context e)
+		  templates' lst
+	and wfgs templates context fgs = tfold templates context wfg fgs
+	and wei templates context lst = 
+	  lfoldl
+		(fun templates ->
+		  fun (str,en,_) ->
+			walk_exp templates context en) templates lst 
+	and wiw templates context = function
+	NEXT_INIT -> templates
+	  | INFIELD_INIT(str,iw) -> wiw templates context iw
+	  | ATINDEX_INIT(en,iw) ->
+		let templates' = walk_exp templates context en in
+		  wiw templates' context iw
+	  | ATINDEXRANGE_INIT(e1,e2) ->
+		let templates' = walk_exp templates context e1 in
+		  walk_exp templates' context e2
+	and wsn templates context (spec,name) =
+	  let templates' = wspecs templates context spec in 
+		wname templates' context name
+	and wsns templates context sns = 
+	  tfold templates context wsn sns
+	and wie templates context = function
+	NO_INIT -> templates
+	  | SINGLE_INIT(en) -> walk_exp templates context en 
+	  | COMPOUND_INIT (iwies) ->
+		lfoldl
+		  (fun templates ->
+			fun (iw,ie) ->
+			  let templates' = wiw templates context iw in
+				wie templates' context ie) templates iwies
+	and wspecs templates context specs = 
 	  lfoldl 
-		(fun templates -> 
-		  fun stmt ->
-			walk_stmt stmt tn_p def_p exp_p arounds guarded_by guarding templates)
-		templates slist
-
-	and walk_stmt sn tn_p def_p exp_p (arounds :dummyNode Set.t) (guarded_by : guard list) (guarding : dummyNode Set.t) templates = 
-	  pprintf "In walk_stmt\n"; flush stdout;
-	  let catchify exp = failwith "Catchify not implemented" in
-	  let diff_tree_node = hfind cabs_id_to_diff_tree_node sn.id in
+		(fun templates ->
+		  fun se -> 
+			match se with
+			  SpecAttr(attr) -> wattr templates context attr
+			| SpecType(ts) ->
+			  (match ts with (* FIXME: strings *)
+			  | Tunion(str, Some(fgo), attrs) 
+			  | Tstruct(str,Some(fgo),attrs) ->
+				let templates' = wfgs templates context fgo in 
+				  wattrs templates' context attrs
+			  | Tenum(str,None,attrs)
+			  | Tunion(str,None, attrs) 
+			  | Tstruct(str,None,attrs) -> wattrs templates context attrs
+			  | Tenum(str,Some(eno),attrs) ->
+				let templates' = wei templates context eno in 
+				  wattrs templates' context attrs
+			  | TtypeofE(en) -> walk_exp templates context en
+			  | TtypeofT(spec,dt) ->
+				let templates' = wspecs templates context spec in 
+				  wdt templates' context dt
+			  | _ -> templates)
+			| _ -> templates) templates specs
+	and wdt templates context dt = 
+	  match dt with
+	  | PARENTYPE(attrs1,dt,attrs2) ->
+		let templates' = wattrs templates context attrs1 in 
+		let templates'' = wdt templates' context dt in
+		  wattrs templates'' context attrs2
+	  | ARRAY(dt,attrs,en) ->
+		let templates' = wdt templates context dt in
+		let templates'' = wattrs templates' context attrs in 
+		  walk_exp templates'' context en
+	  | PTR(attrs,dt) -> 
+		let templates' = wattrs templates context attrs in 
+		  wdt templates' context dt
+	  | PROTO(dt,sns,_) ->
+		let templates' = wdt templates context dt in
+		  wsns templates' context sns
+	  | _ -> templates
+	and win templates context (nme,ie) = 
+	  let templates' = wname templates context nme in 
+		wie templates' context ie
+	and wins templates context ins = tfold templates context win ins
+	and wing templates context (spec,ins) =
+	  let templates' = wspecs templates context spec in
+		wins templates' context ins
+	and wng templates context (spec,names) = 
+	  let templates' = wspecs templates context spec in
+		tfold templates' context wname names
+	and newc c lst = {c with surrounding = Set.union c.surrounding (Set.of_enum (List.enum lst))}
+	and walk_stmts t c lst = 
+	  tfold t (newc c (make_dum_stmt lst)) walk_stmt lst
+	and walk_exps t c lst = 
+	  tfold t (newc c (make_dum_exp lst)) walk_exp lst
+	and walk_defs t c lst = 
+	  tfold t (newc c (make_dum_def lst)) walk_def lst
+	and walk_stmt templates context stmt =
+	  let diff_tree_node = hfind cabs_id_to_diff_tree_node stmt.id in
 	  let stmt_p = 
 		match diff_tree_node.original_node with
 		  STMT(s) -> Some(s) 
 		| _ -> failwith "Expected statement dummyNode, got something else"
 	  in
+	  let context = {context with parent_statement=stmt_p} in
 	  let templates = 
 		if hmem context_ht diff_tree_node.nid then (* FIXME: guarding needs to be changed! I think *)
-		  (make_context tn_p def_p stmt_p exp_p arounds guarded_by  (Set.empty),hfind context_ht diff_tree_node.nid) ::templates
+		  (context,hfind context_ht diff_tree_node.nid) ::templates
 		else templates
 	  in
-	  let wdef def = walk_def def tn_p stmt_p exp_p arounds guarded_by guarding templates in
-	  let wstmt ?guarded_by:(guarded_by=guarded_by) ?templates:(templates=templates) stmt = walk_stmt stmt tn_p def_p exp_p arounds guarded_by guarding templates in
-	  let wstmts ?guarded_by:(guarded_by=guarded_by) ?templates:(templates=templates) slist = walk_stmt_list slist tn_p def_p exp_p arounds guarded_by guarding templates in
-	  let wexp ?guarding:(guarding=guarding) ?templates:(templates=templates) exp = walk_exp exp tn_p def_p stmt_p arounds guarded_by guarding templates in
-
-		match sn.node with
+	  let wexp = walk_exp templates context in
+	  let wstmt = walk_stmt templates context in
+	  let wdef = walk_def templates context in 
+	  let wstmts = walk_stmts templates context in
+		match stmt.node with
 		| COMPUTATION(expn,_) -> wexp expn
 		| BLOCK(blk,_) -> wstmts blk.bstmts
 		| SEQUENCE(s1,s2,_) -> wstmts [s1;s2]
 		| IF(e1,s1,s2,_) -> 
-		  let templates' =
-			wexp ~guarding:(Set.union (Set.singleton (STMT(s1))) guarding) e1 in
-		  let templates'' =
-			wstmt ~guarded_by:((EXPG(e1))::guarded_by) ~templates:templates' s1 
-		  in
-			wstmt ~guarded_by:((OPP(e1))::guarded_by) ~templates:templates'' s2
+		  let context1 = {context with guarding = Set.union (Set.singleton (STMT(s1))) context.guarding} in
+		  let context_t = {context1 with guarded_by = (EXPG(e1)::context1.guarded_by)} in
+		  let context_f = {context1 with guarded_by = (OPP(e1)::context1.guarded_by)} in
+		  let templates' = walk_exp templates context_t e1 in
+		  let templates'' = walk_stmt templates' context_t s1 in
+			walk_stmt templates'' context_f s2
 		| WHILE(e1,s1,_) 
 		| DOWHILE(e1,s1,_) -> 
-		  let templates' = wexp ~guarding:(Set.union (Set.singleton (STMT(s1))) guarding) e1 in
-			wstmt ~guarded_by:(EXPG(e1)::guarded_by) ~templates:templates' s1
+		  let context_e = {context with guarding = (Set.union (Set.singleton (STMT(s1))) context.guarding)} in
+		  let context_s = {context with guarded_by = (EXPG(e1)::context.guarded_by)} in
+		  let templates' = walk_exp templates context_e e1 in
+			walk_stmt templates' context_s s1
 		| FOR(fc,e1,e2,s1,_) ->
+		  let context_e2 = {context with guarding=(Set.union (Set.singleton (STMT(s1))) context.guarding)} in
+		  let context_s1 = {context with guarded_by=(EXPG(e1)::context.guarded_by)} in
 		  let templates' = match fc with
-			  FC_EXP(e3) -> failwith "Not implemented"
-			| FC_DECL(d) -> failwith "Not implemented"
+			  FC_EXP(e3) -> wexp e3
+			| FC_DECL(d) -> wdef d
 		  in
-		  let templates'' = wexp e1 ~templates:templates' in
-			
-		  let templates''' = wexp ~templates:templates'' ~guarding:(Set.union (Set.singleton (STMT(s1))) guarding) e2 in
-			wstmt ~guarded_by:(EXPG(e1)::guarded_by) ~templates:templates''' s1 
+		  let templates'' = walk_exp templates' context e1 in
+		  let templates''' = walk_exp templates'' context_e2 e2 in
+			walk_stmt templates''' context_s1 s1
 		| RETURN(e1,_) -> wexp e1 
 		| SWITCH(e1,s1,_) ->
 		  let templates' = wexp e1 in
-			wstmt ~templates:templates' s1 
+			walk_stmt templates' context s1 
 		| CASE(e1,s1,_) -> 
 		  (* FIXME: check the "guarded" and "guarded_by" on the case statements *)
 		  let templates' = wexp e1 in
-			wstmt ~templates:templates' s1 
+			walk_stmt templates' context s1
 		| CASERANGE(e1,e2,s1,_) ->
 		  let templates' = wexp e1 in
-		  let templates'' = wexp ~templates:templates' e2 in
-			wstmt ~templates:templates'' s1
+		  let templates'' = walk_exp templates' context e2 in
+			walk_stmt templates'' context s1
 		| DEFAULT(s1,_) -> wstmt s1 
 		(* FIXME: walking the strings? *)
 		| LABEL(str,s1,_) -> wstmt s1 
 		| GOTO(str,_) -> templates (* FIXME *)
 		| COMPGOTO(e1,_) ->	wexp e1
 		| DEFINITION(def) -> wdef def
-		| ASM(slist,strs,asdets,_) -> failwith "Screw ASM"
+		| ASM(alist,strs,asdets,_) -> 
+		  let templates' = wattrs templates context alist in
+		  let asmdets templates = 
+			lfoldl
+			  (fun templates ->
+				fun (so,s,en) ->
+				  walk_exp templates context en)
+			  templates in
+			(match asdets with Some(asdets) -> 
+			let templates'' = asmdets templates' asdets.aoutputs in
+			  asmdets templates'' asdets.ainputs
+			| None -> templates')
 		| TRY_EXCEPT(b1,e1,b2,_)->
 		  let templates' =  wstmts b1.bstmts in
-		  let templates'' = wexp ~guarding:(Set.of_enum (List.enum (make_dum_stmt b2.bstmts))) ~templates:templates' e1 in
-			wstmts ~guarded_by:((catchify e1)::guarded_by) ~templates:templates'' b2.bstmts
+		  let context_e1 = {context with guarding=Set.of_enum (List.enum (make_dum_stmt b2.bstmts))} in
+		  let templates'' = walk_exp templates' context_e1 e1 in
+		  let context_s1 = {context with guarded_by = (CATCH(e1)::context.guarded_by)} in
+			walk_stmts templates'' context_s1 b2.bstmts
 		| TRY_FINALLY(b1,b2,_) ->
 		  let templates' = wstmts b1.bstmts in 
-			wstmts ~templates:templates' b2.bstmts 
+			walk_stmts templates' context b2.bstmts 
 		| _ -> templates
-	and walk_def def tn_p stmt_p exp_p (arounds : dummyNode Set.t) (guarded_by : guard list) (guarding : dummyNode Set.t) templates = 
-	  pprintf "in walk def\n"; flush stdout;
+	and walk_def templates context def =
 	  let diff_tree_node = hfind cabs_id_to_diff_tree_node def.id in
 	  let def_p = match diff_tree_node.original_node with
 		  DEF(d) -> Some(d) 
 		| _ -> failwith "Expected def dummyNode, found something else"
 	  in
+	  let context = {context with parent_definition=def_p} in
 	  let templates = 
 		if hmem context_ht diff_tree_node.nid then
-		  (make_context tn_p def_p stmt_p exp_p arounds guarded_by guarding,hfind context_ht diff_tree_node.nid) :: templates
+		  (context,hfind context_ht diff_tree_node.nid) :: templates
 		else templates
 	  in
-		failwith "Not implemented"
-	(*		  match def.node with
-			  FUNDEF of single_name * block * cabsloc * cabsloc
-			  | DIRECTIVE of directive node
-			  | DECDEF of init_name_group * cabsloc        (* global variable(s), or function prototype *)
-			  | TYPEDEF of name_group * cabsloc
-			  | ONLYTYPEDEF of specifier * cabsloc
-			  | GLOBASM of string * cabsloc
-			  | PRAGMA of expression node * cabsloc
-			  | LINKAGE of string * cabsloc * definition node list (* extern "C" { ... } *)*)
-	and walk_exp exp tn_p def_p stmt_p arounds guarded_by guarding templates = 
-	  pprintf "In walk_exp: %d\n" exp.id; flush stdout;
+	  let wexp = walk_exp templates context in
+		match def.node with
+		  FUNDEF(sn,b,_,_) -> walk_stmts templates context b.bstmts
+		| DIRECTIVE(dn) -> templates (* FIXME *)
+		| DECDEF(ing,_) -> wing templates context ing
+		| TYPEDEF(ng,_) -> wng templates context ng
+		| ONLYTYPEDEF(spec,_) -> wspecs templates context spec
+		| GLOBASM(_) -> templates
+		| PRAGMA(en,_) -> wexp en
+		| LINKAGE(_,_,dlist) -> tfold templates context walk_def dlist  (* FIXME: add to "surrounding" *)
+	and walk_exp templates context exp =
 	  let diff_tree_node = hfind cabs_id_to_diff_tree_node exp.id in
-		pprintf "Found it?\n"; flush stdout;
 	  let exp_p = 
 		match diff_tree_node.original_node with
 		  EXP(e) -> Some(e) 
 		| _ -> failwith "Expected expression dummyNode, got something else"
 	  in
+	  let context = {context with parent_expression=exp_p} in
 	  let templates = 
 		if hmem context_ht diff_tree_node.nid then
-		  (make_context tn_p def_p stmt_p exp_p arounds guarded_by guarding,hfind context_ht diff_tree_node.nid) ::templates
+		  (context,hfind context_ht diff_tree_node.nid) ::templates
 		else templates
 	  in
-	  let wdef def = walk_def def tn_p stmt_p exp_p arounds guarded_by guarding templates in
-	  let wstmt ?guarded_by:(g=guarded_by) ?templates:(t=templates) stmt = walk_stmt stmt tn_p def_p exp_p arounds guarded_by guarding templates in
-	  let wstmts ?guarded_by:(g=guarded_by) ?templates:(t=templates) slist = walk_stmt_list slist tn_p def_p exp_p arounds guarded_by guarding templates in
-	  let wexp ?guarding:(g=guarding) ?templates:(t=templates) exp = walk_exp exp tn_p def_p stmt_p arounds guarded_by guarding templates in
-
+	  let wstmts = walk_stmts templates context in
+	  let wexp = walk_exp templates context in
 		match exp.node with
 		| MEMBEROF(e1,str)  (* FIXME: strs *)
 		| MEMBEROFPTR(e1,str) -> wexp e1
@@ -257,26 +354,34 @@ let treediff_to_templates (tree1 : tree) (difftree1 : diff_tree_node) (tdiff : c
 		| INDEX(e1,e2)
 		| BINARY(_,e1,e2) ->
 		  let templates' = wexp e1 in
-			wexp ~templates:templates' e2
+			walk_exp templates' context e2
 		| QUESTION(e1,e2,e3) ->
 		  let templates' = wexp e1 in
-		  let templates'' = wexp ~templates:templates' e2 in
-			wexp ~templates:templates'' e2
-		(*		| CAST of (specifier * decl_type) * init_expression
-				| CALL of expression node * expression node list
-				| COMMA of expression node list
-				| VARIABLE(str) -> templates (* FIXME *)
-				| TYPE_SIZEOF of specifier * decl_type
-				| TYPE_ALIGNOF of specifier * decl_type
-				| GNU_BODY of block*)
+		  let templates'' = walk_exp templates' context e2 in
+			walk_exp templates'' context e2
+		| CAST((spec,dt),ie) -> 
+		  let templates' = wspecs templates context spec in
+		  let templates'' = wdt templates' context dt in 
+			wie templates'' context ie
+		| CALL(e1,elist) ->
+		  let templates' = wexp e1 in 
+			walk_exps templates' context elist
+		| COMMA(elist) -> walk_exps templates context elist
+		| VARIABLE(str) -> templates (* FIXME *)
+		| TYPE_SIZEOF(spec,dt) 
+		| TYPE_ALIGNOF(spec,dt) ->
+		  let templates' = wspecs templates context spec in
+			wdt templates' context dt
+		| GNU_BODY(b) -> wstmts b.bstmts
 		| _ -> templates
 	in
 	let (fname,tns) = tree1 in
-		lfoldl
-		  (fun templates -> 
-			fun tn -> (* FIXME: this is wrong in the handling of befores and afters; deal with teh fact that we can start in stmts or something *)
-			  walk_treenode tn (Set.empty) [] templates) []
-		  tns
+	let initial_context = make_context None None None None (Set.empty) [] (Set.empty) in
+	  lfoldl
+		(fun templates -> 
+		  fun tn -> (* FIXME: this is wrong in the handling of befores and afters; deal with teh fact that we can start in stmts or something *)
+			walk_treenode templates initial_context tn) []
+		tns
   
 (* location can be a function, right, that takes changes and applies them to the
    right part of the tree *)
