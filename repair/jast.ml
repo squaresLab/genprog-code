6bfa840bdc9de4032366794ee32f378c2f7cd6ee
Claire Le Goues <claire.legoues@gmail.com>
2011-06-16 18:16:19 +0000

    didn't mean to change jast

--- repair/jast.ml
+++ repair/jast.ml
@@ -29,6 +29,7 @@ let _ =
   options := !options @
   [
     "--atomizer-loc", Arg.Set_string path_to_atomizer, "X path to javaatomizer.py";
+    "--file-list-path", Arg.Set_string file_list_path, " X use X as the list of files to compile" ;
     "--debug-jast-mutations", Arg.Set debug_jast_mutations, " print debug info on mutation failures (breaks the GA search strategy)" (*fixme*);
     "--debug-java-parser", Arg.Set debug_parser_flag, " print debug info on parser operation"
   ] 
