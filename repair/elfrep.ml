00f7ce45f0057ec51ff4fdcb47d966d40d7630c0
Eric Schulte <eschulte@cs.unm.edu>
2011-10-28 03:21:59 +0000

    fixed this nonsense with arrays of lists, addresses, offsets and indexing out of bounds

--- repair/elfrep.ml
+++ repair/elfrep.ml
@@ -140,17 +140,29 @@ class elfRep = object (self : 'self_type)
                !ins_sizes)
   end
 
+  method show_bytes () =
+    if false then begin
+      debug "(";
+      Array.iter
+        (fun lst ->
+           debug "("; List.iter (fun byte -> debug "%d " byte;) lst; debug ")";)
+        !bytes;
+      debug ")\n";
+    end
+
+  method flat_bytes () = (Array.of_list(List.flatten(Array.to_list !bytes)))
+
   method from_source (filename : string) = begin
     path := filename;
     address := text_address filename;
     offset := text_offset filename;
     bytes := self#bytes_of filename;
+    self#show_bytes();
   end
-  
+
   method output_source source_name = begin
     ignore (Unix.system ("cp " ^ !path ^ " " ^ source_name));
-    update_text source_name !offset
-      (Array.of_list (List.flatten (Array.to_list !bytes)));
+    update_text source_name !offset (self#flat_bytes())
   end
 
   method internal_compute_source_buffers () =
@@ -188,8 +200,8 @@ class elfRep = object (self : 'self_type)
       debug "elf: %s has old version\n" filename ;
       failwith "version mismatch"
     end ;
-    debug "ERROR: might not be able to load serialized object\n" ;
-    path := Marshal.from_channel fin ;
+    debug "ERROR: might not be able to load serialized object, bailing...\n" ;
+    path := exit 1;
     address := text_address !path;
     offset := text_offset !path;
     bytes := self#bytes_of !path;
@@ -199,21 +211,25 @@ class elfRep = object (self : 'self_type)
 
   method max_atom () = (Array.length !bytes) - 1
 
+  method address_offset_to_instruction line =
+    let byte_offset = ref(line - !address) in
+    let instr_offset = ref 0 in
+      Array.iter (fun lst ->
+                    if (!byte_offset > 0) then begin
+                      byte_offset := (!byte_offset - (List.length lst));
+                      instr_offset := (!instr_offset + 1)
+                    end) !bytes;
+      !instr_offset
+
   (* convert a memory address into a genome index *)
   method atom_id_of_source_line source_file source_line =
-    (* TODO: address is raw byte index, transform this into instruction index *)
-    let line = source_line - !address in
-      if line < 0 || line > self#max_atom () then begin
-        debug "elfrep: bad line access %d:%d\n" source_line line;
+    let instruction_id = self#address_offset_to_instruction(source_line) in
+      if instruction_id < 0 || instruction_id > self#max_atom () then begin
+        debug "elfrep: bad line access %d\n" instruction_id;
         0
       end
       else
-        line
-
-  (* convert a genome index into a memory address *)
-  method source_line_of_atom_id (atom_id : int) =
-    (* TODO: reverse instruction <-> byte index transformation *)
-    atom_id + !address
+        instruction_id
 
   method load_oracle oracle_file =
 	failwith "elf: no oracle fix localization"
@@ -223,7 +239,6 @@ class elfRep = object (self : 'self_type)
 
   method get_compiler_command () =
     "__COMPILER_NAME__ __SOURCE_NAME__ __EXE_NAME__ 2>/dev/null >/dev/null"
-
 
   method get_coverage coverage_sourcename coverage_exename coverage_outname =
     (* the use of two executable allows oprofile to sample the pos
@@ -244,7 +259,7 @@ class elfRep = object (self : 'self_type)
             let res, _ = (self#internal_test_case neg_exe
                             coverage_sourcename (Negative i)) in
               if res then begin
-                (* debug "ERROR: coverage PASSES test Negative %d\n" i ; *)
+                debug "ERROR: coverage PASSES test Negative %d\n" i ;
               end ;
           done ;
         done ;
@@ -267,11 +282,11 @@ class elfRep = object (self : 'self_type)
             List.sort (fun (a,_) (b,_) -> a - b) !res in
         let filter_by_bounds (samples : (int * float) list) =
           let results = ref [] in
-          let size = Array.length !bytes in
+          let size = Array.length (self#flat_bytes()) in
             List.iter
               (fun (addr, count) ->
-                 let index = addr - !address in
-                   if (0 <= index) && (index <= size) then
+                 let index = addr in
+                   if (!address <= index) && (index <= (!address + size)) then
                      results := (index,count) :: !results ;) samples ;
             List.sort (fun (a,_) (b,_) -> a - b) !results in
         (* let drop_to counts file path = *)
@@ -315,7 +330,8 @@ class elfRep = object (self : 'self_type)
     end
 
   method debug_info () = begin
-    debug "elf: lines = %d\n" (self#max_atom ());
+    debug "elf: lines=%d bytes=%d\n"
+      (self#max_atom ()) (Array.length (self#flat_bytes()));
   end
 
   method atom_to_byte atom = List.map int_of_string atom
@@ -327,100 +343,96 @@ class elfRep = object (self : 'self_type)
   method get_genome () =
     List.map self#byte_to_atom (Array.to_list !bytes)
 
- method set_genome new_g =
+  method set_genome new_g =
     bytes := Array.of_list (List.map self#atom_to_byte new_g)
 
   method get ind =
-    if (ind <= self#max_atom ()) then
-      self#byte_to_atom (Array.get !bytes ind)
-    else
-      []
+    let i = self#address_offset_to_instruction ind in
+      self#byte_to_atom (Array.get !bytes i)
 
   method put ind newv =
-    if (ind <= self#max_atom ()) then begin
-      super#put ind newv;
-      Array.set !bytes ind (self#atom_to_byte newv)
-    end
+    let i = self#address_offset_to_instruction ind in
+      super#put i newv;
+      Array.set !bytes i (self#atom_to_byte newv)
 
   method swap i j =
-    let max = Array.length !bytes in
-      if (i < max) && (j < max) then begin
-        super#swap i j;
-        let temp = Array.get !bytes i in
-          Array.set !bytes i (Array.get !bytes j) ;
-          Array.set !bytes j temp ;
-      end
+    let i_i = self#address_offset_to_instruction i in
+    let j_i = self#address_offset_to_instruction j in
+      super#swap i_i j_i;
+      let temp = Array.get !bytes i_i in
+        Array.set !bytes i_i (Array.get !bytes j_i) ;
+        Array.set !bytes j_i temp ;
+        self#show_bytes() ;
 
   method delete i =
-    let max = Array.length !bytes in
-      if (i < max) then begin
-        super#delete i ;
-        let num = List.length (Array.get !bytes i) in
-        let len = Array.length !bytes in
-        let rep = Array.make num (if !elf_risc then [0; 0; 160; 225] else [144]) in
-          if (i == 0) then
-            bytes := Array.append rep (Array.sub !bytes 1 (len - 1))
-          else if (i == (len - 1)) then
-            bytes := Array.append (Array.sub !bytes 0 (len - 1)) rep
-          else
-            bytes := Array.append
-              (Array.append (Array.sub !bytes 0 i) rep)
-              (Array.sub !bytes (i + 1) ((len - i) - 1)) ;
-      end
+    let i_i = self#address_offset_to_instruction i in
+      super#delete i_i ;
+      let num = List.length (Array.get !bytes i_i) in
+      let len = Array.length !bytes in
+      let rep = Array.make num (if !elf_risc then [0; 0; 160; 225] else [144]) in
+        if (i_i == 0) then
+          bytes := Array.append rep (Array.sub !bytes 1 (len - 1))
+        else if (i_i == (len - 1)) then
+          bytes := Array.append (Array.sub !bytes 0 (len - 1)) rep
+        else
+          bytes := Array.append
+            (Array.append (Array.sub !bytes 0 i_i) rep)
+            (Array.sub !bytes (i_i + 1) ((len - i_i) - 1)) ;
+        self#show_bytes() ;
 
   method append i j =
-    let max = Array.length !bytes in
-      if (i < max) && (j < max) then begin
-        super#append i j ;
-        let inst = ref (Array.get !bytes j) in
-        let reps = ref (List.length !inst) in
-          (* append new instruction into the array *)
-          bytes := Array.concat [
-            (Array.sub !bytes 0 i);
-            [|!inst|];
-            (Array.sub !bytes i ((Array.length !bytes) - i));
-          ] ;
-          (* delete an appropriate amount of nop's *)
-          let max x y = if x > y then x else y in
-            for p = 0 to max i ((Array.length !bytes) - i) do
-              if (!reps > 0) then begin
-                try
-                  match Array.get !bytes (i+p) with
-                    | [0; 0; 160; 225] when !elf_risc -> begin
-                        reps := !reps - 4 ;
-                        Array.set !bytes (i+p) []
-                      end
-                    | [144] when (not !elf_risc) -> begin
-                        reps := !reps - 1 ;
-                        Array.set !bytes (i+p) []
-                      end
-                    | _     -> begin
-                        match Array.get !bytes (i-p) with
-                          | [0; 0; 160; 225] when !elf_risc -> begin
-                              reps := !reps - 4 ;
-                              Array.set !bytes (i-p) []
-                            end
-                          | [144] when (not !elf_risc) -> begin
-                              reps := !reps - 1 ;
-                              Array.set !bytes (i-p) []
-                            end
-                          | _ -> ()
-                      end
-                with
-                  | Invalid_argument  "index out of bounds" -> ()
-                  | Invalid_argument  "Array.sub" -> ()
-              end
-            done ;
-            (* if still too long, then truncate *)
-            if (!reps > 0) then
-              bytes := Array.sub !bytes 0 ((Array.length !bytes) - !reps) ;
-            (* remove empty instruction strings *)
-            bytes := Array.of_list
-              (List.rev (Array.fold_left
-                           (fun a e -> match e with
-                              | [] -> a
-                              | _  -> e :: a)
-                           [] !bytes)) ;
-      end
+    let i_i = self#address_offset_to_instruction i in
+    let j_i = self#address_offset_to_instruction j in
+      super#append i_i j_i ;
+      let inst = ref (Array.get !bytes j_i) in
+      let reps = ref (List.length !inst) in
+        (* append new instruction into the array *)
+        bytes := Array.concat [
+          (Array.sub !bytes 0 i_i);
+          [|!inst|];
+          (Array.sub !bytes i_i ((Array.length !bytes) - i_i));
+        ] ;
+        (* delete an appropriate amount of nop's *)
+        let max x y = if x > y then x else y in
+          for p = 0 to max i_i ((Array.length !bytes) - i_i) do
+            if (!reps > 0) then begin
+              try
+                match Array.get !bytes (i_i+p) with
+                  | [0; 0; 160; 225] when !elf_risc -> begin
+                      reps := !reps - 4 ;
+                      Array.set !bytes (i_i+p) []
+                    end
+                  | [144] when (not !elf_risc) -> begin
+                      reps := !reps - 1 ;
+                      Array.set !bytes (i_i+p) []
+                    end
+                  | _     -> begin
+                      match Array.get !bytes (i_i-p) with
+                        | [0; 0; 160; 225] when !elf_risc -> begin
+                            reps := !reps - 4 ;
+                            Array.set !bytes (i_i-p) []
+                          end
+                        | [144] when (not !elf_risc) -> begin
+                            reps := !reps - 1 ;
+                            Array.set !bytes (i_i-p) []
+                          end
+                        | _ -> ()
+                    end
+              with
+                | Invalid_argument  "index out of bounds" -> ()
+                | Invalid_argument  "Array.sub" -> ()
+            end
+          done ;
+          (* if still too long, then truncate *)
+          if (!reps > 0) then
+            bytes := Array.sub !bytes 0 ((Array.length !bytes) - !reps) ;
+          (* remove empty instruction strings *)
+          bytes := Array.of_list
+            (List.rev (Array.fold_left
+                         (fun a e -> match e with
+                            | [] -> a
+                            | _  -> e :: a)
+                         [] !bytes)) ;
+          self#show_bytes();
 
 end
