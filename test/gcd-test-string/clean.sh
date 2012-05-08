49b45dd75da18faaec15e956677e1bdbfbebb499
Neal Holtschulte <neal.holts@cs.unm.edu>
2012-05-08 16:01:16 +0000

    Small changes and tidying up trunk/test/ and subfolders.

---
+++ test/gcd-test-string/clean.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+rm -rf *sanity* *debug* 00*
+rm -f  n1 p1 p2 p3 p4 p5
+rm -f *cache
+rm -f repair.*
+rm -f coverage.*
+rm -f *.ast
+rm -f *.ht
+rm -f *.path
+rm -f *-coverage.c
+rm -f *-baseline.c
+rm -f coverage coverage.c coverage.path.neg coverage.path.pos
