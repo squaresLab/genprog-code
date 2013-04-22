f8e9d92866569fdf5b6642939a401b82fac4bf9e
Eric Schulte <eschulte@cs.unm.edu>
2013-04-22 19:44:07 +0000

    stub out llvmrep.ml

---
+++ src/llvmrep.ml
@@ -0,0 +1,175 @@
+(*
+ *
+ * Copyright (c) 2012-2013,
+ *  Wes Weimer          <weimer@cs.virginia.edu>
+ *  Stephanie Forrest   <forrest@cs.unm.edu>
+ *  Eric Schulte        <eschulte@cs.unm.edu>
+ *  Claire Le Goues     <legoues@cs.virginia.edu>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. The names of the contributors may not be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *)
+(** LLVM IR representation *)
+
+open Printf
+open Global
+open Rep
+
+let llvmRep_version = "1"
+
+class llvmRep = object (self : 'self_type)
+
+  (** holds the name of the `llvm-mutate' excutable **)
+  val llvm_mutate = "llvm-mutate"
+
+  (** no fault localization *)
+  inherit [string list, string list] faultlocRepresentation as super
+
+  (** the genome for llvmRep is a single large string *)
+  val genome = ref ""
+
+  (** by default, llvmRep variants are of fixed length *)
+  method variable_length = true
+
+  method get_genome () = failwith "llvmRep: black box genome"
+  method set_genome g = failwith "llvmRep: black box genome"
+  method genome_length () = failwith "llvmRep: black box genome"
+
+  method copy () : 'self_type =
+    let super_copy : 'self_type = super#copy () in
+      super_copy#internal_copy ()
+
+  method internal_copy () : 'self_type =
+    {< genome = ref (Global.copy !genome) ; >}
+
+  method from_source (filename : string) =
+    genome := Std.input_file filename
+
+  method to_source (filename : string) =
+    Std.output_file filename !genome
+
+  (* internal_compute_source_buffers can theoretically overflow the buffer if
+     the rep is extremely large *)
+  method internal_compute_source_buffers () =
+      [ None, copy !genome ]
+
+  method serialize ?out_channel ?global_info (filename : string) =
+    let fout =
+      match out_channel with
+      | Some(v) -> v
+      | None -> open_out_bin filename
+    in
+      Marshal.to_channel fout (llvmRep_version) [] ;
+      Marshal.to_channel fout (!genome) [] ;
+      debug "llvmRep: %s: saved\n" filename ;
+      super#serialize ~out_channel:fout ?global_info:global_info filename ;
+      if out_channel = None then close_out fout
+
+  (* load in serialized state.  Deserialize can fail if the file from which the
+     rep is being read does not conform to the expected format, or if the
+     version written to that file does not match the current rep version. *)
+  method deserialize ?in_channel ?global_info (filename : string) =
+    let fin =
+      match in_channel with
+      | Some(v) -> v
+      | None -> open_in_bin filename
+    in
+    let version = Marshal.from_channel fin in
+      if version <> llvmRep_version then begin
+        debug "llvmRep: %s has old version\n" filename ;
+        failwith "version mismatch"
+      end ;
+      genome := Marshal.from_channel fin ;
+      debug "llvmRep: %s: loaded\n" filename ;
+      super#deserialize ~in_channel:fin ?global_info:global_info filename ;
+      if in_channel = None then close_in fin
+
+  (* Run an llvm-mutate command with a temporary source file *)
+  method run cmd =
+    let tmp_from = Filename.temp_file "llvmRepFrom" ".ll" in
+    let tmp_to = Filename.temp_file "llvmRepTo" ".ll" in
+    let cmd = "cat "^tmp_from^"|"llvm_mutate^" "^cmd^">"^tmp_to in
+    let cleanup () =
+      if Sys.file_exists tmp_from then Sys.remove source_file;
+      if Sys.file_exists tmp_to then Sys.remove preprocessed in
+    (* write to tmp_from *)
+    self#to_source tmp_from;
+    (* run the command *)
+    let return =
+      match (Unix.system cmd) with
+      | Unix.WEXITED(0) -> true
+      | _ -> false in
+    (* read from tmp_from *)
+    self#from_source tmp_to;
+    cleanup();
+    return
+
+  method max_atom () =
+    let tmp_from = Filename.temp_file "llvmRepFrom" ".ll" in
+    let cleanup () =
+      if Sys.file_exists tmp_from then Sys.remove source_file; in
+    (* write to tmp_from *)
+    self#to_source tmp_from;
+    (* run the command *)
+    let return = Unix.system "cat "^tmp_from^"|"llvm_mutate^" -I" in
+    cleanup();
+    return
+
+  method atom_id_of_source_line source_file source_line =
+    (* throw an error *)
+
+  method instrument_fault_localization _ _ _ =
+    failwith "llvmRep: no fault localization"
+
+  method debug_info () = debug "llvmRep: lines = 1--%d\n" (self#max_atom ())
+
+  (* the rep-modifying methods, like get,put, swap, append, etc, do not do error
+     checking on their arguments to guarantee that they are valid indices into the
+     rep array.  Thus, they may fail *)
+  method get idx = failwith "llvmRep: `get' is not supported"
+
+  method put idx newv = failwith "llvmRep: `put' is not supported"
+
+  method swap i j =
+    super#swap i j ;
+    (* shell command to swap *)
+
+  method delete i =
+    super#delete i ;
+    (* shell command to delete *)
+
+  method append i j =
+    super#append i j ;
+    (* shell command to append *)
+
+  method replace i j =
+    super#replace i j ;
+    (* shell command to replace *)
+
+end
