9293ace0a1517b742a46dffb64e18eb0eaaca1e5
Claire Le Goues <clegoues@cs.cmu.edu>
2015-04-14 14:39:30 +0000

    atom_id_of_source_line has been wrong for a long time, it seems.  Now returns a list of atoms instead of a singleton.

--- src/asmrep.ml
+++ src/asmrep.ml
@@ -145,13 +145,13 @@ class asmRep = object (self : 'self_type)
   method atom_id_of_source_line source_file source_line =
     (* return the in-code offset from the global offset *)
     if !asm_code_only then
-      List.fold_left (+) 0 (List.map (fun (a,b) ->
+      [List.fold_left (+) 0 (List.map (fun (a,b) ->
         if a > source_line then
           if b > source_line then b - a
           else source_line - a
-        else 0) !range)
+        else 0) !range)]
     else
-      source_line
+      [source_line]
 
   method source_line_of_atom_id atom_id =
     (* return global offset from in-code offset *)
